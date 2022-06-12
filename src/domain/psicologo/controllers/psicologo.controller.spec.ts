c7f60b491ae164ec94daaef4c5fdfca1ea9615a6
Gefferson Oliveira <gefferson_oliveira@outlook.com>
2022-06-12 16:42:42 -0300

    CORREÇÕES: Arquivo jest.config.ts movido para dentro de src para evitar erro ao tentar compilar o typescript. ADICIONADO: Melhorado teste de integração create do psicologo.

--- src/domain/psicologo/controllers/psicologo.controller.spec.ts
+++ src/domain/psicologo/controllers/psicologo.controller.spec.ts
@@ -1,27 +1,70 @@
 import supertest from "supertest";
 import app from "../../..";
 
-describe('No controller, ao executar functions', () => {
+describe('No controller, ao executar função', () => {
 
     describe('create', () => {
+        const emailTest = "5512" ;
+
         test('em caso de sucesso, deve retornar 201', async () => {
-
             const response = await supertest(app)
-
             .post('/psicologos')
-
             .send({
-                nome: "aloha",
-                email: "test1@gmail.com", //UNIQUE
+                nome: "Status1",
+                email: ("test"+emailTest+"a@gmail.com"),
+                senha: "12345678",
+                apresentacao: "bla bla bla",
+                cep: "01001000"
+            })
+    
+            expect(response.status).toBe(201)
+        })
+
+        test('em caso de sucesso, retornar response do request', async () => {
+            const response = await supertest(app)
+            .post('/psicologos')
+            .send({
+                nome: "Body2",
+                email: ("test"+emailTest+"b@gmail.com"),
                 senha: "123",
                 apresentacao: "bla bla bla",
                 cep: "01001000"
             });
+    
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    createdAt: expect.any(String),
+                    id: expect.any(Number),
+                    nome: expect.any(String),
+                    email: expect.any(String),
+                    senha: expect.any(String),
+                    apresentacao: expect.any(String),
+                    bairro: expect.any(String),
+                    updatedAt: expect.any(String),
+                })
+            );
 
-            expect(response.status).toBe(201);
-            expect(response.body.nome).toBe("aloha");
+            expect(JSON.parse(response.text)).toMatchObject(
+                {
+                    nome: "Body2",
+                    email: ("test"+emailTest+"b@gmail.com"), 
+                })
+
+            expect(response.body).toMatchObject(
+             {
+                nome: "Body2",
+                email: ("test"+emailTest+"b@gmail.com"), 
+            });
+
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    nome: "Body2",
+                    email: ("test"+emailTest+"b@gmail.com"),
+                    apresentacao: "bla bla bla",
+                })
+            );
         })
 
-    })
 
+    })
 })
